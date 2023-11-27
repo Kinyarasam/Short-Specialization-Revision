40e1a1c12d033a21a4f29ab50b1eabaa2e8e7ede
kinyarasam <skinyara.30@gmail.com>
2023-11-27 17:50:11 +0300

    Read and filter data

--- alx-backend-user-data/filtered_logger.py
+++ alx-backend-user-data/filtered_logger.py
@@ -3,9 +3,11 @@
 filtered_logger File
 """
 
+import os
 import logging
 import typing
 import re
+import mysql.connector
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -28,7 +30,7 @@ class RedactingFormatter(logging.Formatter):
         txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
         return txt
 
-def filter_datum(fields: typing.List, redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
     """
     Function to Obfuscate a message
 
@@ -42,8 +44,8 @@ def filter_datum(fields: typing.List, redaction: str, message: str, separator: s
         (String): obfuscated message.
     """
     for field in fields:
-        pattern = re.search(r'{f}=(.*?){s}'.format(f=field,s=separator), message).group(0).split('=')[1][0:-1]
-        message = re.sub(pattern, redaction, message)
+        pattern = re.search('{f}=(.*?){s}'.format(f=field,s=separator), message).group(0).split('=')[1][0:-1]
+        message = re.sub(re.escape(pattern), redaction, message)
     return message
 
 def get_logger() -> logging.Logger:
@@ -65,3 +67,48 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Creates a connector to the Database
+
+    Returns:
+        The connector to the database
+    """
+    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'hbnb_revision')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME', "")
+    db_pwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
+    )
+    return connection
+
+
+def main():
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(",")
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row))
+            msg = '; '.join(list(record))
+            log_record = logging.LogRecord(
+                "user_data", logging.INFO, None, None, msg, None, None
+            )
+            info_logger.handle(log_record)
+
+
+if __name__ == '__main__':
+    main()
