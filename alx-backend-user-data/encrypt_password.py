b826d6fe30331469b2970e3c6a87e630e237ce5e
kinyarasam <skinyara.30@gmail.com>
2023-11-28 12:47:46 +0300

    Check valid password

--- alx-backend-user-data/encrypt_password.py
+++ alx-backend-user-data/encrypt_password.py
@@ -14,3 +14,15 @@ def hash_password(password: str) -> bytes:
     """
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates the provided password matches the hashed password
+
+    Args:
+        hashed_password (bytes): The hashed password to be checked.
+        password (str): The password to be validated.
+
+    Returns:
+        bool: True if the password is valid, False otherwise
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
