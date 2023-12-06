65207c55aa97600d52df707f7112fd7efddc52ca
kinyarasam <skinyara.30@gmail.com>
2023-12-06 17:06:26 +0300

    Basic - User object

--- alx-backend-user-data/0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ alx-backend-user-data/0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Definition of BasicAuth
 """
-from api.v1.auth.auth import Auth
 import base64
 import re
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -48,3 +50,21 @@ class BasicAuth(Auth):
             return (None, None)
 
         return tuple(decoded_base64_authorization_header.strip().split(":"))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for u in users:
+                if u.is_valid_password(user_pwd):
+                    return u
+            return None
+        except Exception:
+            return None
