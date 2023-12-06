34c9e4974e36e629642a52bcf2afda08f742a658
kinyarasam <skinyara.30@gmail.com>
2023-12-06 14:35:59 +0300

    Define which routes don't need authentication

--- alx-backend-user-data/0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ alx-backend-user-data/0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -3,10 +3,19 @@
 """
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        if path is None or not isinstance(excluded_paths, list) or len(excluded_paths) < 1:
+            return True
+
+        for p in excluded_paths:
+            match = re.match(path, p)
+            if not match:
+                return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
