a55b4fa7111b06ede15048255aff38ab8ed4dd49
kinyarasam <skinyara.30@gmail.com>
2023-12-14 12:55:59 +0300

    Before request

--- alx-backend-user-data/0x02-Session_authentication/SimpleAPI/api/v1/app.py
+++ alx-backend-user-data/0x02-Session_authentication/SimpleAPI/api/v1/app.py
@@ -34,12 +34,15 @@ def before_request():
         excluded = [
             '/api/v1/status/'
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
 
         if auth.require_auth(path=request.path, excluded_paths=excluded):
-            if auth.authorization_header(request=request) is None:
+            cookie = auth.session_cookie(request=request)
+            if auth.authorization_header(request=request) is None and cookie is None:
                 abort(401, description="Unauthorized")
+
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
 
