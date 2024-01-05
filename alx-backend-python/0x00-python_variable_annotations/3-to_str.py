e7d0f9cf62fdc006460c1313994b0bb40fc4944d
kinyarasam <skinyara.30@gmail.com>
2024-01-05 11:36:40 +0300

    3. Basic annotations - to string

---
+++ alx-backend-python/0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" to_str module
+"""
+
+
+def to_str(n: float) -> str:
+    """ Returns a string representation of a float
+
+    Args:
+        n (float): the number
+
+    Return:
+        (str): string representation of a float value
+    """
+    return str(n)
