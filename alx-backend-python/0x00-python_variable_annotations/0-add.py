1031543cb7a47ccb79559f5fd194af7e37fe103e
kinyarasam <skinyara.30@gmail.com>
2024-01-05 11:19:01 +0300

    0. Basic annotations - add

---
+++ alx-backend-python/0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" add module
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ functions that adds to numbers.
+
+    Args:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        (float): sum of the two numbers
+    """
+    return a + b
