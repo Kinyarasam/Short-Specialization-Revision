3cd4f54e9fc243b3b5145d510951d84170943523
kinyarasam <skinyara.30@gmail.com>
2024-01-05 11:30:46 +0300

    1. Basic annotations - concat

---
+++ alx-backend-python/0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" concat module
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ cancatenates two strings
+
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        (str): The concatenated string
+    """
+    return str1 + str2
