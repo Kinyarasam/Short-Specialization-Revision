271c8afd1f125bf54af537b9fb77805467608b33
kinyarasam <skinyara.30@gmail.com>
2024-01-05 11:31:13 +0300

    2. Basic annotations - floor

---
+++ alx-backend-python/0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" floor module
+"""
+
+
+def floor(n: float) -> int:
+    """ Rounds of the number to the nearest whole number
+
+    Args:
+        n (float): the number
+
+    Returns:
+        (int): the floored number
+    """
+    return int(n)
